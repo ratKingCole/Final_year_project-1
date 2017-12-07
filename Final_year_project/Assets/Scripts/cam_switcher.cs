c1982fb06d135a1e7b7410c9eee7cbaac2c97773
ratKingCole <ethanbirch42@gmail.com>
2017-12-07 20:23:04 +0000

    cam update

--- Final_year_project/Assets/Scripts/cam_switcher.cs
+++ Final_year_project/Assets/Scripts/cam_switcher.cs
@@ -1,56 +1,172 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class cam_switcher : MonoBehaviour {
+public class cam_switcher : MonoBehaviour
+{
+    public Transform startMarker;
+    public float speed;
+    private float startTime;
+    public bool moving = false;
+    public string cameraName = "";
+    public string currentCamera = "";
+    public string previousCamera  = "";
+    public int stage;
+    public bool update = true;
+    public bool onload = true;
 
-    public Camera firstperson;
-    public Camera overhead;
-    public Camera thirdperson;
-    public Camera cue;
-
-    void Start() {
-        firstperson.enabled = false;
-        overhead.enabled = true;
-        thirdperson.enabled = false;
-        cue.enabled = false;
-    }
-
-    void Update(){
-        if (Input.GetKey(KeyCode.Alpha1)) ShowOverheadView();
-        if (Input.GetKey(KeyCode.Alpha2)) ShowThirdPersonView();
-        if (Input.GetKey(KeyCode.Alpha3)) ShowFirstPersonView();
-        if (Input.GetKey(KeyCode.Alpha4)) ShowcueView();
-    }
-
-    public void ShowcueView()
+    void Start() { }
+ 
+    void Update()
     {
-        cue.enabled = true;
-        firstperson.enabled = false;
-        overhead.enabled = false;
-        thirdperson.enabled = false;
-    }
-    public void ShowOverheadView(){
-        cue.enabled = false;
-        firstperson.enabled = false;
-        overhead.enabled = true;
-        thirdperson.enabled = false;
+
+        if (onload)
+        {
+            cameraName = "cue";
+            moving = true;
+            onload = false;
+            speed = 5f;
+            startMarker = transform;
+            startTime = Time.time;
+        }
+        if (!moving)
+        {
+            speed = 5f;
+            startMarker = transform;
+            startTime = Time.time;
+            if (update)
+            {
+                previousCamera = currentCamera;
+                update = false;
+            }
+           
+            cameraName = "";
+            stage = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1) || (moving && cameraName == "overhead"))
+        {
+            update = true;
+            cameraName = "overhead";
+            currentCamera = cameraName;
+            moveCam(startMarker.position, new Vector3(0, 30, 0), GameObject.Find("Table").transform.position);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2) || (moving && cameraName == "firstperson"))
+        {
+            update = true;
+            cameraName = "firstperson";
+            currentCamera = cameraName;
+            moveCam(startMarker.position, GameObject.Find("cueBall(Clone)").transform.position, new Vector3(0, 0, 10));
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3) || (moving && cameraName == "thirdperson"))
+        {
+            update = true;
+            cameraName = "thirdperson";
+            currentCamera = cameraName;
+            Vector3 pos = GameObject.Find("cueBall(Clone)").transform.position;
+            Vector3 end = new Vector3(pos.x, pos.y + 5, pos.z - 5);
+            moveCam(startMarker.position, end, pos);
+            
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4) || (moving && cameraName == "cue"))
+        {
+            update = true;
+            cameraName = "cue";
+            currentCamera = cameraName;
+            if (GameObject.Find("cueBall(Clone)")) { 
+                Vector3 pos = GameObject.Find("cueBall(Clone)").transform.position;
+                Vector3 end = new Vector3(pos.x, pos.y + 5, pos.z - 8);
+                moveCam(startMarker.position, end, pos);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5) || (moving && cameraName == "spin"))
+        {
+            cameraName = "spin";
+            currentCamera = cameraName;
+            Vector3 tag = GameObject.Find("Table").transform.position;
+            Vector3 end1 = new Vector3(0, 20, -20);
+            if (stage == 1) {
+                speed = 10;
+                moveCam(startMarker.position, end1, tag);
+                if (!moving) {
+                    stage++;
+                    moving = true;
+                }
+            }
+            if(stage == 2)
+            {
+                speed = 50;
+                transform.LookAt(GameObject.Find("Table").transform.position);
+                transform.RotateAround(GameObject.Find("Table").transform.position, Vector3.up, speed * Time.deltaTime);
+                moving = true;
+            }
+            if (transform.position.z < 0 && (transform.position.x >= 0.1 && transform.position.x <= 1))
+            {
+                speed = 1;
+                stage++;
+                moveCam(startMarker.position, end1, tag);
+            }
+        }
+        /*
+        if (Input.GetKeyDown(KeyCode.Alpha6) || (moving && cameraName == "spin2"))
+        {
+            speed = 20;
+            cameraName = "spin2";
+            currentCamera = cameraName;
+            transform.LookAt(GameObject.Find("Table").transform.position);
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+            moving = true;
+        }
+        */
+
+        if (Input.GetKeyDown(KeyCode.Alpha6) || (moving && cameraName == "break"))
+        {
+
+            Vector3 tag = GameObject.Find("ballSpawner").transform.position;
+            Vector3 end = new Vector3(tag.x, tag.y + 20, tag.z);
+            speed = .1f;
+            cameraName = "break";
+            currentCamera = cameraName;
+            transform.LookAt(tag);
+            moveCam(startMarker.position, end, tag);
+
+
+
+
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha7) || (moving && cameraName == "pocket"))
+        {
+
+            Vector3 tag = GameObject.Find("pocket (3)").transform.position;
+            Vector3 end = new Vector3(tag.x+5, tag.y + 10, tag.z-5);
+            speed = .1f;
+            cameraName = "pocket";
+            currentCamera = cameraName;
+            transform.LookAt(tag);
+            moveCam(startMarker.position, end, tag);
+
+
+
+
+
+        }
     }
 
-    public void ShowFirstPersonView()
+    void moveCam(Vector3 start, Vector3 end, Vector3 target)
     {
-        cue.enabled = false;
-        firstperson.enabled = true;
-        overhead.enabled = false;
-        thirdperson.enabled = false;
-    }
-
-    public void ShowThirdPersonView()
-    {
-        cue.enabled = false;
-        firstperson.enabled = false;
-        overhead.enabled = false;
-        thirdperson.enabled = true;
+        float distance = Vector3.Distance(start, end);
+        if (distance == 0)
+        {
+            moving = false;
+        }
+        else
+        {
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / distance;
+            transform.position = Vector3.Lerp(start, end, fracJourney);
+            transform.LookAt(target);
+            if (fracJourney > 1) moving = false;
+            else moving = true;
+        }
     }
 
 }

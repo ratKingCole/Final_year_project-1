f0c74a428f6f67673155900b00303e26fc150778
Goofus <anthonyprocell@gmail.com>
2017-12-08 00:19:53 +0000

    Working in-game menu and Extended menu features.

--- Final_year_project/Assets/Scripts/ChangeScene.cs
+++ Final_year_project/Assets/Scripts/ChangeScene.cs
@@ -2,8 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ChangeScene : MonoBehaviour {
+    public Button btnRes1;
+    public Button btnRes2;
+    public Button btnRes3;
+    public Button btnBack;
+
+    public Button btnPlay;
+    public Button btnSettings;
+    public Button btnExit;
+
+    public void ChangeRes(int resolution)
+    {
+        switch (resolution)
+        {
+            case 1:
+                Screen.SetResolution(1920, 1080, true);
+                break;
+            case 2:
+                Screen.SetResolution(1600, 900, true);
+                break;
+            case 3:
+                Screen.SetResolution(1280, 720, true);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    public void BackButton()
+    {
+        btnPlay.gameObject.SetActive(true);
+        btnSettings.gameObject.SetActive(true);
+        btnExit.gameObject.SetActive(true);
+
+        btnRes1.gameObject.SetActive(false);
+        btnRes2.gameObject.SetActive(false);
+        btnRes3.gameObject.SetActive(false);
+        btnBack.gameObject.SetActive(false);
+    }
+
+    public void SettingsButton()
+    {
+        btnPlay.gameObject.SetActive(false);
+        btnSettings.gameObject.SetActive(false);
+        btnExit.gameObject.SetActive(false);
+
+        btnRes1.gameObject.SetActive(true);
+        btnRes2.gameObject.SetActive(true);
+        btnRes3.gameObject.SetActive(true);
+        btnBack.gameObject.SetActive(true);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
 
 	public void LoadScene(string sceneName)
     {
